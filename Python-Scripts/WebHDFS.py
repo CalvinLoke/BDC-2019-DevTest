c653f4674f4ffc5265be049a1702ba880c6e2234
unknown <CalvinLke@github.com>
2021-01-11 18:12:48 +0800

    Basic skeleton

--- Python-Scripts/WebHDFS.py
+++ Python-Scripts/WebHDFS.py
@@ -1,6 +1,45 @@
 import requests
 import urllib3
 
-user_input = input("Please enter a value: ")
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-print(user_input)
+def createDirectory(dir_name):
+
+    params = (
+        ('op', 'MKDIRS'),
+    )
+
+    # url = r'https://10.10.0.104:30443/gateway/default/webhdfs/v1/' + dir_name
+    url = r'https://10.10.0.104:30443/gateway/default/webhdfs/v1/test_dir'
+
+    response = requests.put(url, params=params, verify=False, auth=('admin', 'Password1234'))
+    
+    print(response)
+
+
+def uploadFile():
+
+    headers = {
+        'Content-Type': 'application/octet-stream',
+    }
+
+    params = (
+        ('op', 'create'),
+    )
+
+    response = requests.put('https://10.10.0.104:30443/gateway/default/webhdfs/v1/test_dir/test2.csv', headers=headers, params=params, verify=False, auth=('admin', 'Password1234'))
+
+    print(response)
+
+
+def checkDirectory(path_to_dir):
+    params = (
+        ('op', 'liststatus'),
+    )
+
+    response = requests.get('https://10.10.0.104:30443/gateway/default/webhdfs/v1/test_dir/', params=params, verify=False, auth=('admin', 'Password1234'))
+
+    print(response.content)
+
+
+checkDirectory("null")
