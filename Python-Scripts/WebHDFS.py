3cee3bf2c7c2cdec4552b71a2b55c49e62e75d02
CalvinLoke <70929524+CalvinLoke@users.noreply.github.com>
2021-01-15 17:54:48 +0800

    Update WebHDFS.py
    Attempted fix for uploading large files

--- Python-Scripts/WebHDFS.py
+++ Python-Scripts/WebHDFS.py
@@ -50,22 +50,22 @@ def uploadFile(path, file_name):
 
     # response = requests.put('https://10.10.0.104:30443/gateway/default/webhdfs/v1/test/test.csv?op=create', headers=headers, verify=False, auth=('admin', 'Password1234'))
 
-    # response = requests.put(url,
-    #                         data=open(file_name,'rb'),
-    #                         headers=headers,
-    #                         params=params,
-    #                         verify=False,
-    #                         auth=('admin', 'Password1234'))
-
-    response = requests.post(url,
+    response = requests.put(url,
                             data=open(file_name,'rb'),
                             headers=headers,
                             params=params,
                             verify=False,
                             auth=('admin', 'Password1234'))
 
+    # response = requests.post(url,
+    #                         data=open(file_name,'rb'),
+    #                         headers=headers,
+    #                         params=params,
+    #                         verify=False,
+    #                         auth=('admin', 'Password1234'))
+
     print(response.status_code)
-    print(response.text)
+    print(response.content)
 
 
 def checkDirectory(folder_name):
