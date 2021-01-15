fafcb77bb1513ee74214ba8217a91f519cadc8bc
CalvinLoke <70929524+CalvinLoke@users.noreply.github.com>
2021-01-15 14:24:53 +0800

    Update WebHDFS.py
    Testing code

--- Python-Scripts/WebHDFS.py
+++ Python-Scripts/WebHDFS.py
@@ -37,8 +37,10 @@ def uploadFile(path, file_name):
 
     # https://10.10.0.104:30443/gateway/default/webhdfs/v1/
 
+    # response = requests.put(url, headers=headers, params=params, verify=False, auth=('admin', 'Password1234'))
 
-    response = requests.put(url, headers=headers, params=params, verify=False, auth=('admin', 'Password1234'))
+    response = requests.put('https://10.10.0.104:30443/gateway/default/webhdfs/v1/test/test.csv?op=create', headers=headers, verify=False, auth=('admin', 'Password1234'))
+
     # response = requests.post()
 
     print(response)
